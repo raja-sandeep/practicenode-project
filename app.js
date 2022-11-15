73074a16cf5fd7326400c9fb3d0122ebfbece4b9
raja-sandeep <rajasandeepvuba@gmail.com>
2022-11-15 23:04:25 +0530

    added something

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ let express = require("express");
 
 let bodyparser = require("body-parser");
 
-let routes=require('./routes')
+let routes = require("./routes");
 
 let app = express();
 let port = 5009;
 
 app.listen(port, () => {
+  console.log("app listening on port 5007");
 
-console.log("app listening on port 5007")
+  console.log("added new console");
+});
 
+app.use(bodyparser.json());
 
-})
-
-app.use(bodyparser.json())
-
-routes(app)
+routes(app);
