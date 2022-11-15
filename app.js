718191b6f31fa51463c3dee463df5506b2c17d63
raja-sandeep <rajasandeepvuba@gmail.com>
2022-11-16 00:23:27 +0530

    some changes on student.js

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ let bodyparser = require("body-parser");
 let routes = require("./routes");
 
 let app = express();
-let port = 5009;
+let port = 5002;
 
 app.listen(port, () => {
-  console.log("app listening on port 5007");
-console.log("sssssssssssssssssssssssssssssssssssssssssssssss")
-  console.log("added new console");
+  console.log("app listening on port 5002");
 });
 
 app.use(bodyparser.json());
